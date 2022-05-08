d740c62641c6198d28b18c7004aab0af65f42320
NoellyCosta <59965675+NoellyCosta@users.noreply.github.com>
2022-05-08 13:47:56 -0300

    Manipulating values

---
+++ manipulatingAnimationValues/script.js
@@ -0,0 +1,13 @@
+// levar para meio triangular
+
+// anime({
+//     targets: '.square',
+//    translateX: '+=300',
+//    translateY: '+=300',
+// })
+
+
+anime({
+    targets: '.square',
+    translateX: anime.random(100, 300),
+})
